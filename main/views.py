6444af98e8fbf914ca83e9c22a2227316113d53d
imadeddinedjekoune <imaddjekoune@gmail.com>
2023-09-15 13:07:43 -0700

    comm14

--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(request):
 	print(request.user_agent.os)
 	print(request.META.get('REMOTE_ADDR'))
-	return render(request,"main/index.html")
+	return render(request,"main/test.html")
