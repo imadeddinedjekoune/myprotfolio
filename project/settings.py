1fa5d8828189cebb13eefe5ce14948d42eefe15f
imadeddinedjekoune <imaddjekoune@gmail.com>
2023-09-15 04:37:06 -0700

    comm5

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1c)$&gk(tmc$-)gm!m#0)@yduv$ttbdkueonx42-u#do-)m_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["imadeddine-9d01ff1b3f83.herokuapp.com","http://127.0.0.1/","www.djekouneimad.com","djekouneimad.com"]
+ALLOWED_HOSTS = ["imadeddine-9d01ff1b3f83.herokuapp.com","127.0.0.1","www.djekouneimad.com","djekouneimad.com"]
 
 
 # Application definition
